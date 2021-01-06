213cf2e740b47feb988ac7bb8bd2b3149272dcf4
MR-ZIA-BHI <77040212+MR-ZIA-BHI@users.noreply.github.com>
2021-01-06 10:25:37 +0500

    Update KomailKhan.py

--- KomailKhan.py
+++ KomailKhan.py
@@ -142,8 +142,8 @@ print  """\033[1;91m
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "komail"
-CorrectPassword = "khan"
+CorrectUsername = "syed"
+CorrectPassword = "zia"
 
 loop = 'true'
 while (loop == 'true'):
